eab290f23225949e1a924f6940ccbf55e4a7bdf1
KshitijAucharmal <kshitijaucharmal21@gmail.com>
2020-12-14 19:19:05 +0530

    added nn

--- DQN/CartPole-try1.py
+++ DQN/CartPole-try1.py
@@ -1,16 +1,22 @@
 import numpy as np
 import gym
+from nn import neuralNetwork
 
 env = gym.make("CartPole-v0")
+model = neuralNetwork(3, 10, 1, 0.3)
 
-for i in range(10):
-    env.reset()
-    done = False
-    rew = 0
-    while not done:
-        env.render()
-        next_state, reward, done, _ = env.step(env.action_space.sample())
-        rew += reward
-    print(f"Episode {i} has {rew} rewards")
+def main():
+    for i in range(10):
+        env.reset()
+        done = False
+        rew = 0
+        while not done:
+            env.render()
+            next_state, reward, done, _ = env.step(env.action_space.sample())
+            rew += reward
+        print(f"Episode {i} has {rew} rewards")
 
-env.close()
+    env.close()
+
+if __name__ == "__main__" && input("Press ENTER To Start") == "":
+    main()
