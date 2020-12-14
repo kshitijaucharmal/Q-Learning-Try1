4feac1a9fb443a52fbb7322cd82f15a43443d4e5
KshitijAucharmal <kshitijaucharmal21@gmail.com>
2020-12-14 12:43:57 +0530

    First working program with Q Learning training and testing (Will add options for users later)

--- main.py
+++ main.py
@@ -12,27 +12,56 @@ def clear():
     else:
         _ = system('clear')
 
-EPISODES = 4
+def main():
+    EPISODES = 1000
 
-env = gym.make("Taxi-v3")
+    env = gym.make("Taxi-v3")
 
-Q = np.zeros((env.observation_space.n, env.action_space.n))
-R = env.P[328]
-print(R)
-tot_rew = []
-for i in range(EPISODES):
-    env.reset()
-    done = False
-    rew = 0
-    while not done:
-        env.render()
-        next_state, reward, done, _ = env.step(env.action_space.sample())
-        print(f"Iteration {i} Done {done}")
-        rew += reward
-        # sleep(0.02)
+    Q = np.zeros((env.observation_space.n, env.action_space.n))
+
+    EPSILON = 0.6
+    GAMMA = 0.99
+    ALPHA = 0.6
+
+    tot_rew = []
+    for i in range(EPISODES):
+        state = env.reset()
+        done = False
+        rew = 0
+        while not done:
+            action = env.action_space.sample() if np.random.random() < EPSILON else np.argmax(Q[state])
+            next_state, reward, done, _ = env.step(action)
+            Q[state, action] += ALPHA * (reward + GAMMA * np.max(Q[next_state]) - Q[state, action])
+            state = next_state
+            rew += reward
+        print(f"Interation {i} with rewards {rew}")
         clear()
-    tot_rew.append(rew)
+        tot_rew.append(rew)
 
-if(input("Press Enter to get info and exit") == ''):
-    print(f"Average Rewards from {EPISODES} episodes is {np.sum(tot_rew)/len(tot_rew)}")
-    env.close()
+    if(input("Press Enter to get info") == ''):
+        print(f"Average Rewards from {EPISODES} episodes is {np.sum(tot_rew)/len(tot_rew)}")
+        print("Q table : ")
+        print(Q)
+
+    if(input("Press Enter to continue or q to quit") == "q"):
+        exit()
+
+    tot_rew = []
+    for i in range(10):
+        state = env.reset()
+        done = False
+        rew = 0
+        while not done:
+            env.render()
+            action = np.argmax(Q[state])
+            next_state, reward, done, _ = env.step(action)
+            rew += reward
+            state = next_state
+            sleep(0.1)
+            clear()
+        print(f"Iteration {i} with reward {rew}")
+        tot_rew.append(rew)
+    print("Total Reward after training {}".format(sum(tot_rew) / len(tot_rew)))
+
+if __name__ == "__main__":
+    main()
